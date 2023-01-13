4402013e6be7a535352421b38b58e68260e26d68
abhisheksiingh <singhabhishek123114@gmail.com>
2023-01-13 22:56:57 +0530

    dd

--- app.js
+++ app.js
@@ -135,6 +135,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(80, function() {
+app.listen(10000, function() {
   console.log("Server started on port 3000");
 });
